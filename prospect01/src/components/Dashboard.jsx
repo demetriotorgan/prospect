4f8d33984d8056fec326ddcb4737fb3a2bef9c7c
demetriotorgan <demetriotorgan@hotmail.com>
2025-09-03 14:58:23 -0300

    template01

---
+++ prospect01/src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import '../styles/Dashboard.css'
+import Navbar from './Navbar/Navbar'
+import Main from './Main/Main'
+
+const Dashboard = () => {
+
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    </>
+  )
+}
+
+export default Dashboard

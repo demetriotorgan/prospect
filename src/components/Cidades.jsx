f00979126aa3b710c5a23ff3af3448ac8b1842bf
demetriotorgan <demetriotorgan@hotmail.com>
2025-09-17 09:26:20 -0300

    listarNichosPorCidades

--- src/components/Cidades.jsx
+++ src/components/Cidades.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import '../styles/Cidades.css'
 
 const Cidades = () => {
   return (
-    <div>Cidades</div>
+    <div className='painel-cidades'>
+      <div className='selecionar-cidade'>        
+        <select>
+          <option>Cidades</option>
+          <option>Nova Londrina</option>
+          <option>Maringá</option>
+          <option>Paranavaí</option>
+        </select>        
+      </div>
+      <div className='painel-cards-cidades'>
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+          <div className='card-cidade'>
+            <h3>Nicho: Mecânica</h3>
+            <p>Empresas Cadastradas: <span>25</span></p>
+            <p>Empresas Prospectadas: <span>18</span></p>
+            <p>Não prospectadas: <span>7</span></p>
+            <p>Agendamentos: <span>10</span></p>
+            <p>Indefinidos: <span>5</span></p>
+            <p>Sem Interesse: <span>3</span></p>
+          </div>          
+      </div>
+    </div>
   )
 }
 
